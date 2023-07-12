b100230450e89df15e679dd8e977082526f42b4e
Chi Karl <kchi8573@gmail.com>
2023-07-12 11:14:48 +0100

    Fixed the Access across sites

--- functions/airtable.js
+++ functions/airtable.js
@@ -34,6 +34,9 @@ exports.handler = async (event, context, cb) => {
       }
     })
     return {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
       statusCode: 200,
       body: JSON.stringify(products),
     }
